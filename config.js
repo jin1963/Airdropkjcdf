531862e1a6a05849f8910d04371d865f94164b1d
KAOJNO DRAGON CHAIN <kaojino89@gmail.com>
2026-02-19 13:54:59 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.APP_CONFIG = {
+  CHAIN_ID_HEX: "0x38",
+  CHAIN_NAME: "BSC Mainnet",
+  RPC_URL: "https://bsc-dataseed.binance.org/",
+  EXPLORER: "https://bscscan.com",
+
+  KJC: "0x2FB9b0F45278D62dc13Dc9F826F78e8E3774047D",
+  DF:  "0x36579d7eC4b29e875E3eC21A55F71C822E03A992",
+};
